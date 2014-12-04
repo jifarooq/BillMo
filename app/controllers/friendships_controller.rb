3676b394221797db8aa193e9b49060dd3b36dcbc
Justin Farooq <justinfarooq@gmail.com>
2014-12-04 08:12:56 -0800

    changed friendship index

---
+++ app/controllers/friendships_controller.rb
@@ -0,0 +1,23 @@
+class FriendshipsController < ApplicationController
+	def create
+		@friendship = current_user.friendships.new(friendship_params)
+		@friendship.try(:save)
+		render json: @friendship
+	end
+
+	def destroy
+		@friendship = current_user.friendships.find(params[:id])
+		@friendship.try(:destroy)
+		render json: {}
+	end
+
+	def index
+		@friendships = current_user.friendships
+		render json: @friendships
+	end
+
+  private
+  	def friendship_params
+  		params.require(:friendship).permit(:user_id, :friend_id)
+  	end
+end
