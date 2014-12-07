fbb126b9069daf247b9a1d57f364d1263d03baa6
Justin Farooq <justinfarooq@gmail.com>
2014-12-06 23:36:48 -0800

    fixed balance update bug

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -14,6 +14,6 @@ class Api::UsersController < ApplicationController
 
 	private
 		def user_params
-			params.require(:current_user).permit(:balance)
+			params.require(:user).permit(:balance)
 		end
 end
